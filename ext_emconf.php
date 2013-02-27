af9b92c15e5dbdd45605243e6b943ae775838889
Jonathan Marshall <jonathan@globalinbox.co>
2013-02-27 21:51:02 +0200

    Initial add of extension. Version 1.8.1 downloaded from http://tinyrte.outraxx.de/

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "tinyrte".
+#
+# Auto generated 03-10-2011 20:40
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'tinyRTE',
+	'description' => 'RTE based on TinyMCE. Supported Browsers: Mozilla, MSIE, FireFox, Opera and Safari.',
+	'category' => 'be',
+	'shy' => 0,
+	'version' => '1.8.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
+	'module' => 'mod1,mod2',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => 'be_users,be_groups',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Thomas Schulze',
+	'author_email' => 'kontakt@outraxx.de',
+	'author_company' => '[outraxX]',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
